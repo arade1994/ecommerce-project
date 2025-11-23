d32b24f1fe09a28f545454b2ce87cdefcbe08aa6
Ante Rade <anterade99@gmail.com>
2025-11-23 23:08:50 +0100

    Initial commit

---
+++ backend/spring-boot-ecommerce/src/main/java/com/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.dao;
+
+
+import com.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
