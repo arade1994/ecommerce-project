f6cc5c31015b2297b229ceb76ebee75ea30bca4c
Ante Rade <anterade99@gmail.com>
2025-11-26 16:28:58 +0100

    Implemented new ui

--- frontend/angular-ecommerce/src/app/services/product.ts
+++ frontend/angular-ecommerce/src/app/services/product.ts
@@ -3,11 +3,17 @@ import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Product } from '../common/product';
 
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  };
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  private baseUrl = 'http://localhost:8080/api/products';
+  private baseUrl = 'http://localhost:8080/api/products?size=100';
 
   constructor(private httpClient: HttpClient) {}
 
@@ -16,10 +22,4 @@ export class ProductService {
       .get<GetResponse>(this.baseUrl)
       .pipe(map((response) => response._embedded.products));
   }
-}
-
-interface GetResponse {
-  _embedded: {
-    products: Product[];
-  };
 }
